3987646a3666944a5aaf68fe01b9938fdab5853b
Hellboy94520 <alexandre.delahaye.94@gmail.com>
2021-11-06 17:41:15 +0100

    Playing with module rsc.io and quote package

--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// Requirement: 'go get rsc.io/quote' and generated go.sum file
+	"rsc.io/quote"
+)
 
 func main() {
-	fmt.Println("Hello World !")
+	fmt.Println("Quote package from module rsc.io:")
+	fmt.Println("- quote.Go()	  : " + quote.Go())
+	fmt.Println("- quote.Glass(): " + quote.Glass())
+	fmt.Println("- quote.Hello(): " + quote.Hello())
+	fmt.Println("- quote.Opt()  : " + quote.Opt())
 }
