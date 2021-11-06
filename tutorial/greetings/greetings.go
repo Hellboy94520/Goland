f3211a1a4314735ef7e9b04d298743979757d9d7
Hellboy94520 <alexandre.delahaye.94@gmail.com>
2021-11-06 18:08:52 +0100

    Handle an error

--- tutorial/greetings/greetings.go
+++ tutorial/greetings/greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// Checking input arg
+	if name == "" {
+		// Do not use capital letter or punctuation (ST1005)
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- tutorial/hello/hello.go
+++ tutorial/hello/hello.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	/* Import my local module locally instead of remote :
 	 * 'go mod edit -replace example.com/greetings=../greeting'
 	 * and sync it on hello folder :
@@ -11,7 +13,17 @@ import (
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	// Init Log
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greetings msg with empty string, expected error
+	message, err := greetings.Hello("")
+	if err != nil {
+		// Fatal msg and leaving program
+		log.Fatal(err)
+	}
+
+	// If not error, print msg
 	fmt.Println(message)
 }
